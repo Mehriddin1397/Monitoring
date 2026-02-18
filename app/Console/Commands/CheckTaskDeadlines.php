65a5d0fc7da1287ede2085e5d98c4f1fcea29ec6
Mehriddin1397 <soyibovmehriddin@gmail.com>
2026-02-18 09:53:59 +0500

    task_type2

--- app/Console/Commands/CheckTaskDeadlines.php
+++ app/Console/Commands/CheckTaskDeadlines.php
@@ -16,37 +16,37 @@ class CheckTaskDeadlines extends Command
     public function handle(EskizSmsService $smsService): int
     {
         $today = now()->toDateString();
-//        $twoDaysLater = now()->addDays(2)->toDateString();
+        $twoDaysLater = now()->addDays(2)->toDateString();
 
         // 2 kun qolgan tasklar
-//        $tasks = Task::whereDate('end_date', $twoDaysLater)
-//            ->with('assignedUsers:id,tel_number')
-//            ->get();
-//
-//        $userTaskCounts = [];
-//
-//        foreach ($tasks as $task) {
-//            foreach ($task->assignedUsers as $user) {
-//                if (!$user->tel_number) continue;
-//
-//                $userTaskCounts[$user->id]['phone'] = $user->tel_number;
-//                $userTaskCounts[$user->id]['count'] =
-//                    ($userTaskCounts[$user->id]['count'] ?? 0) + 1;
-//            }
-//        }
-//
-//        foreach ($userTaskCounts as $data) {
-//            $smsService->send(
-//                $data['phone'],
-//                "Sizda {$data['count']} ta topshiriq muddati tugamoqda. Topshirishga 2-kun vaqt qoldi."
-//            );
-//        }
+        $tasks = Task::whereDate('end_date', $twoDaysLater)
+            ->with('assignedUsers:id,tel_number')
+            ->get();
+
+        $userTaskCounts = [];
+
+        foreach ($tasks as $task) {
+            foreach ($task->assignedUsers as $user) {
+                if (!$user->tel_number) continue;
+
+                $userTaskCounts[$user->id]['phone'] = $user->tel_number;
+                $userTaskCounts[$user->id]['count'] =
+                    ($userTaskCounts[$user->id]['count'] ?? 0) + 1;
+            }
+        }
+
+        foreach ($userTaskCounts as $data) {
+            $smsService->send(
+                $data['phone'],
+                "Sizda {$data['count']} ta topshiriq muddati tugamoqda. Topshirishga 2-kun vaqt qoldi."
+            );
+        }
 
         // Bugungi topshiriqlar
         $todayCount = Task::whereDate('end_date', $today)->count();
 
-        $adminPhone = '998942551397';
-//        $adminPhone = '998941050078';
+//        $adminPhone = '998942551397';
+        $adminPhone = '998941050078';
 
         $smsService->send(
             $adminPhone,
